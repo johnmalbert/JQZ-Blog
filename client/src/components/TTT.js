c7a816e63dfb2b20f8597bab184fa838973b43ad
QLathe <quinn.s.lathe@gmail.com>
2021-03-29 15:44:12 -0700

    Update TTT.js
    game stops once there is a winner

--- client/src/components/TTT.js
+++ client/src/components/TTT.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 
 export default () => {
+    var win = false;
     const [ttarr, setTtarr] = useState([])
     const checkWin = () => {
         const lines = [
@@ -16,7 +17,7 @@ export default () => {
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i];
             if (ttarr[a] && ttarr[a] === ttarr[b] && ttarr[a] === ttarr[c]) {
-                console.log("win")
+                win = true;
                 return ttarr[a];
             }
         }
@@ -50,34 +51,43 @@ export default () => {
         document.getElementById(turn).innerText="X";
     }
     const onClick = e => {
-        if(!ttarr[e.target.id]){
-            ttarr[e.target.id] = 1;
-            setTtarr(ttarr);
-            e.target.innerText="O";
-            checkWin()
-            oppTurn()
-            console.log(ttarr)
+        if(win===false){
+            if(!ttarr[e.target.id]){
+                ttarr[e.target.id] = 1;
+                setTtarr(ttarr);
+                e.target.innerText="O";
+                checkWin()
+                if(win===false){
+                    oppTurn()
+                }
+                console.log(ttarr)
+            }
         }
     }
     return(
         <div>
+        {
+            win ? 
+            ''
+            : 
             <table id="tttboard">
-                <tr>
-                    <td id="0" onClick={onClick} class="ttt"></td>
-                    <td id="1"  onClick={onClick} class="ttt"></td>
-                    <td id="2" onClick={onClick} class="ttt"></td>
-                </tr>
-                <tr>
-                    <td id="3" onClick={onClick} class="ttt"></td>
-                    <td id="4" onClick={onClick} class="ttt"></td>
-                    <td id="5" onClick={onClick} class="ttt"></td>
-                </tr>
-                <tr>
-                    <td id="6" onClick={onClick} class="ttt"></td>
-                    <td id="7" onClick={onClick} class="ttt"></td>
-                    <td id="8" onClick={onClick} class="ttt"></td>
-                </tr>
-            </table>
+            <tr>
+            <td id="0" onClick={onClick} class="ttt"></td>
+            <td id="1"  onClick={onClick} class="ttt"></td>
+            <td id="2" onClick={onClick} class="ttt"></td>
+            </tr>
+            <tr>
+            <td id="3" onClick={onClick} class="ttt"></td>
+            <td id="4" onClick={onClick} class="ttt"></td>
+            <td id="5" onClick={onClick} class="ttt"></td>
+            </tr>
+            <tr>
+            <td id="6" onClick={onClick} class="ttt"></td>
+            <td id="7" onClick={onClick} class="ttt"></td>
+            <td id="8" onClick={onClick} class="ttt"></td>
+            </tr>
+            </table>    
+        }
         </div>
     )
 }
