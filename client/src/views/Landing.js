1c62327b03aa14fb700222eb79cd09899871902b
John Albert <john.m.albert@gmail.com>
2021-03-30 14:42:24 -0700

    fixed merge conflict

--- client/src/views/Landing.js
+++ client/src/views/Landing.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import DevJoke from '../components/DevJoke'
+import { Link } from '@reach/router';
 
 const Landing = () => {
     return (
         <div>
-<<<<<<< HEAD
             <h1>Landing Page!</h1>
             <h3>Navigation Links:</h3>
-            <div>
                 Will be a dropdown
                 <Link to="/portfolio/john">Portfolio | </Link> 
                 <Link to="/code">Code Snippets | </Link>
                 <Link to="/algorithms">Algorithm Challenges | </Link>
-=======
             <div className="landingJoke">
                 <DevJoke />
->>>>>>> 7f496faf6ec42209d1281bb689ee366d9e687db6
             </div>
         </div>
     )
