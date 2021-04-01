5db1472b8dffb23aec2755c09bf9c104cfd8d263
John Albert <john.m.albert@gmail.com>
2021-04-01 11:46:03 -0700

    portfilio and single portfolio pages modified

--- client/src/components/ProjectComponents/ProjectDisplay.js
+++ client/src/components/ProjectComponents/ProjectDisplay.js
@@ -5,11 +5,8 @@ const ProjectDisplay = props => {
     return (
         <div>
             <h4><a href={project[index].github}>{project[index].title}</a></h4>
-<<<<<<< HEAD
             <p>{project[index].desc} - {project[index].languages}</p>
-=======
             <p>{project[index].desc}</p>
->>>>>>> ba48393 (Project update)
         </div>
     )
 }
