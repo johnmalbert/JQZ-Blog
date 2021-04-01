70cfe90327c1e900ae047a78d3a8fafa57149c23
QLathe <quinn.s.lathe@gmail.com>
2021-04-01 14:58:53 -0700

    show Portfolio projects

--- client/src/components/PortfolioComponents/PortfolioBody.js
+++ client/src/components/PortfolioComponents/PortfolioBody.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import ProjectDisplay from '../ProjectComponents/ProjectDisplay'
+
 // import guy from '../guy.jpg';
 import { Link } from '@reach/router';
 const PortfolioBody = props => {
@@ -7,22 +9,13 @@ const PortfolioBody = props => {
     const [projects, setProjects] = useState([])
     // get the projects owned by this user
     useEffect(() => {
-        axios.get("http://localhost:8000/api/projects")
-            .then(response => {
-                let projectsByOwner = [];
-                let allProjects = response.data.results;
-                for(let i = 0; i < allProjects.length; i++){
-                    console.log(allProjects[i]);
-                    console.log(allProjects[i].ownerId);
-                    if(allProjects[i].ownerId === id){
-                        projectsByOwner.push(allProjects[i]);
-                    }
-                }
-                setProjects(projectsByOwner);
-                console.log(projects);
-            })
-            .catch(err => console.log("Error getting projects", err))
-    },[])
+        axios.get("http://localhost:8000/api/projectsOwned/"+id)
+        .then(response => {
+            setProjects(response.data.results);
+            console.log(projects)
+        })
+        .catch(err => console.log("Can't find that project", err))
+},[])
     return (
         <div>
                 <div className="row">
@@ -35,6 +28,14 @@ const PortfolioBody = props => {
                         </div>
                         <div className="col text-center">
                             <h3>My Technical Projects:</h3>
+                                {projects ?
+                            <div>
+                                {projects.map((task, i) => (
+                                    <ProjectDisplay project={projects} index={i}/>
+                                    ))}
+                            </div>
+                            : ""
+                            }
                             <button className="btn btn-primary"><Link portfolio={portfolio} style={{color: 'white'}} to={`/projects/new/${id}`}> Add a Project</Link></button>
                         </div>
                     </div>
--- client/src/components/ProjectComponents/ProjectDisplay.js
+++ client/src/components/ProjectComponents/ProjectDisplay.js
@@ -4,7 +4,7 @@ const ProjectDisplay = props => {
     const { project, index } = props;
     return (
         <div>
-            <h4><a href={project[index].github}>{project[index].title}</a></h4>
+            <h4><a href={project[index].github} alt="">{project[index].title}</a></h4>
             <p>{project[index].desc} - {project[index].languages}</p>
         </div>
     )
