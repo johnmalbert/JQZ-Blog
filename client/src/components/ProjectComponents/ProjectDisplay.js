61f93e84be6b60f0db7fd266ee6eee7b74fc14e0
Springer114 <springerzack04@yahoo.com>
2021-04-01 12:52:45 -0700

    resolve merge conflict

--- client/src/components/ProjectComponents/ProjectDisplay.js
+++ client/src/components/ProjectComponents/ProjectDisplay.js
@@ -6,10 +6,7 @@ const ProjectDisplay = props => {
         <div>
             <h4><a href={project[index].github}>{project[index].title}</a></h4>
             <p>{project[index].desc} - {project[index].languages}</p>
-<<<<<<< HEAD
             <p>{project[index].desc}</p>
-=======
->>>>>>> cd78e31 (Update ProjectDisplay.js)
         </div>
     )
 }
