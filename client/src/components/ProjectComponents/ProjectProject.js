13d5c422b0f8b47bdb820f8f47dcf3b58806381b
QLathe <quinn.s.lathe@gmail.com>
2021-04-02 12:50:38 -0700

    Project Title Update
    Updated title looks

--- client/src/components/ProjectComponents/ProjectProject.js
+++ client/src/components/ProjectComponents/ProjectProject.js
@@ -5,9 +5,8 @@ const ProjectDisplay = props => {
     const { project, index } = props;
     return (
         <div>
-            <h4><a href={project[index].github} alt="">{project[index].title}</a></h4>
+                <h4><a href={project[index].github} alt="">{project[index].title}</a><span class="projectspan"><Link to={`/portfolio/${project[index].ownerId}`}> - {project[index].owner}</Link></span></h4>
             <p>{project[index].desc} - {project[index].languages}
-            <br/><Link to={`/portfolio/${project[index].ownerId}`}>{project[index].owner}</Link>
             </p>
         </div>
     )
